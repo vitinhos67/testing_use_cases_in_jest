a11486b6ea489bd5e30ca0f5b14715896cd9f1e1
Victor Santos <santtosvictor42@gmail.com>
2023-03-10 18:33:45 -0300

    chore: initial configs and added function for sum and test for her

---
+++ tests/index.spec.ts
@@ -0,0 +1,20 @@
+import exp from 'constants'
+import {sum } from '../src/common/sum'
+describe("Test function sum", () => {
+
+it("Sum Two numbers to be 2", () => {
+    const sumTwoNumbers = sum(1,1)
+    expect(sumTwoNumbers).toBe(2)
+} )
+
+it("should have an console.log property in function sum", () => {
+
+const consoleSpy = jest.spyOn(console, "log")
+sum(1,1)
+
+expect(consoleSpy).not.toHaveBeenCalledTimes(1)
+
+})
+
+
+})
